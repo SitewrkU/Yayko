053fe625cd68161a62b91fa75e9fa177bf5a09ba
BattWrku <Sitesentsii@gmail.com>
2025-04-23 09:53:06 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 export const EggTypes = [
-    { name: 'Стандартне яйце [20]', price: 20, src: './src/pics/dragonEgg.png', constName: 'StandartEggPets'},
-    { name: 'Слизьке яйце [60]', price: 60, src: './src/pics/waterEgg.png', constName: 'SlimeEggPets'}
+    { name: 'Стандартне яйце [20$]', price: 20, src: './src/pics/dragonEgg.png', constName: 'StandartEggPets'},
+    { name: 'Слизьке яйце [60$]', price: 60, src: './src/pics/waterEgg.png', constName: 'SlimeEggPets'}
 ];
 export const StandartEggPets = [
-    { name: 'Звичайний Кіт', chance: 45, rarity: 'Стандартний', price: 6, src: './src/pics/StandartEgg/DefaultCat.png' },
-    { name: 'Срібний Кіт', chance: 25, rarity: 'Рідкісний', price: 10, src: './src/pics/StandartEgg/SilverCat.png' },
+    { name: 'Звичайний Кіт', chance: 45, rarity: 'Стандартний', price: 10, src: './src/pics/StandartEgg/DefaultCat.png' },
+    { name: 'Срібний Кіт', chance: 25, rarity: 'Рідкісний', price: 14, src: './src/pics/StandartEgg/SilverCat.png' },
     { name: 'Водний Кіт', chance: 15, rarity: 'Епічний', price: 25, src: './src/pics/StandartEgg/WaterCat.png' },
     { name: 'Лавовий Кіт', chance: 10, rarity: 'Епічний', price: 50, src: './src/pics/StandartEgg/LavaCat.png' },
     { name: 'Космічний Кіт', chance: 4.5, rarity: 'Легендарний', price: 100, src: './src/pics/StandartEgg/CosmoCat.png' },
@@ -12,7 +12,7 @@ export const StandartEggPets = [
     { name: 'Заражений Кіт', chance: 0.05, rarity: 'Міфічний', price: 2990, src: './src/pics/StandartEgg/InfectedCat.png' }
 ];
 export const SlimeEggPets = [
-    { name: 'Звичайний Слизень', chance: 30, rarity: 'Стандартний', price: 15, src: './src/pics/SlimeEgg/DefSlime.png' },
+    { name: 'Звичайний Слизень', chance: 30, rarity: 'Стандартний', price: 20, src: './src/pics/SlimeEgg/DefSlime.png' },
     { name: 'Фруктовий Слизень', chance: 25, rarity: 'Стандартний', price: 28, src: './src/pics/SlimeEgg/FruitSlime.png' },
     { name: 'Пухнастий Слизень', chance: 20, rarity: 'Рідкісний', price: 40, src: './src/pics/SlimeEgg/FluffySlime.png' },
     { name: 'Кристальний Слизень', chance: 15, rarity: 'Епічний', price: 80, src: './src/pics/SlimeEgg/CrystalSlime.png' }, 
